384534947214e7c856c4c78db11f3a7f70a5074e
cabritapiupiu <luca39760@gmail.com>
2024-10-14 13:06:03 -0300

    FEAT#0003 Se realizaron los primeros puntos del fin y las primera mejoras para el proyecto

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mysql =require('mysql2');
 const api =express();
@@ -17,20 +18,63 @@ db.connect((error)=>{
         return;
     }
     console.log('Conexion exitosa');
-})
+});
 api.get('/',(request, results)=>{
-    results.send("<h1>Api con express</h1>")
-})
+    results.send("<h1>Api con express</h1>");
+}); 
 
 api.get('/estado', (request,results)=>{
     db.query("SELECT * FROM restaurante__estado",(err,resultados)=>{
         if(err){
-            results.status(500).json({message : err.message })
+            results.status(500).json({message : err.message });
             return;
         }
         results.json(resultados);
     });
+ 
+});
 
+
+/**
+ *En este punto del fin de tipo GET traemos todos los datos del plato que el usuario seleccionó.
+ *Para poder mostrar todos estos datos se necesita un token.
+ * token para usar :   5KUTI9cyi0OmElfBfYfRkTtO2Op4LmutQlJ5GyXLOqvqlXIaS80Se3zGR9oIpx3i
+ * seria de esta forma
+ * http://localhost:3000/plato/5KUTI9cyi0OmElfBfYfRkTtO2Op4LmutQlJ5GyXLOqvqlXIaS80Se3zGR9oIpx3i
+ **/
+
+api.get('/plato/:token', (request,results)=>{
+    const  {token} = request.params;
+    db.query('SELECT * FROM restaurante__menu WHERE token = ?',[token],(err,resultados)=>{
+        if(err){
+            results.status(500).json({message : err.message });
+            return;
+        }
+        results.json(resultados);
+    });
+ 
+});
+
+
+api.post('/pedido/:token', (request,results)=>{
+    const  {descripciones} = request.body;
+    const  {menu} = request.body;
+    const  {user} = request.body;
+
+const token = 'tu_token_aqui';
+
+console.log('Token decodificado:', decoded);
+    
+    const {estado} = 1;
+
+    db.query('INSERT INTO restaurante__pedido (descripciones,FK_estado,FK_menu,FK_user,token,creat__add) VALUES (?,?,?,?,?,?)',[descripciones,estado],(err,resultados)=>{
+        if(err){
+            results.status(500).json({message : err.message });
+            return;
+        }
+        results.json(resultados);
+    });
+ 
 });
 
 const PORT = 3000;
