0c22549ca97f80d96d62d4a37e8f7ec7b97feae3
ChoqueIV <sebachoque03@gmail.com>
2024-11-07 19:21:35 -0300

    feat #003 dasad

--- Chaza/restaurante/src/main.jsx
+++ Chaza/restaurante/src/main.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Logueo from "./components/inicioSesion/Logueo.jsx";
-import Registrarse from './components/inicioSesion/Registrarse.jsx';
-import Email from './components/inicioSesion/Email.jsx';
-import App from './components/Landing/App.jsx';
+import Registro from './pages/Registro.jsx';  // Asegúrate de que este sea el archivo correcto
+import Email from './pages/Email.jsx';
+import Landing from './pages/Landing.jsx';  // Asegúrate de que este sea el archivo correcto
+import Login from './pages/Login.jsx';  // Asegúrate de que este sea el archivo correcto
+import Menu from './pages/Menu.jsx';  // Asegúrate de que este sea el archivo correcto
 
-const ENDPOINTS = {
-  LOGIN: "http://localhost:3000/user",
-  REGISTER: "http://localhost:3000/register"
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='Logueo' element={<Logueo endpoint={ENDPOINTS.LOGIN}/>} />
-        <Route path='Registrarse' element={<Registrarse />} />
-        <Route path='Email' element={<Email/>} />
+        <Route path='/' element={<Landing />} />  {/* Ruta para Landing */}
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Registro' element={<Registro />} />
+        <Route path='/Email' element={<Email />} />
+        <Route path='/Menu' element={<Menu />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
