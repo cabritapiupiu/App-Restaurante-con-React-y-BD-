eaa339912300ee613e5ca740a3c441b66557a465
cabritapiupiu <luca39760@gmail.com>
2024-11-08 20:34:28 -0300

    hola

--- Chaza/restaurante/src/main.jsx
+++ Chaza/restaurante/src/main.jsx
@@ -13,8 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Landing />} />  {/* Ruta para Landing */}
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Login' element={<Login />} />
+        <Route path='/Login' element={<Login/>} />
+        
         <Route path='/Registro' element={<Registro />} />
         <Route path='/Email' element={<Email />} />
         <Route path='/Menu' element={<Menu />} />
