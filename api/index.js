bb39bd3f8243e8fb647e15bfe113a8ad7ffb3d4b
Luca_Cabral <128231997+cabritapiupiu@users.noreply.github.com>
2024-11-15 01:53:23 +0000

    FIX#0003 mejoras en los puntos del fin de menu carrito y de pedido

--- api/index.js
+++ api/index.js
@@ -19,6 +19,27 @@ db.connect((error)=>{
     console.log('Conexion exitosa');
 })
 //------------------------GET-------------------------------------
+
+//http://localhost:3000/tipo
+api.post('/tipo', (request, results) => {
+    const {tipo} = request.body;
+
+    db.query('CALL get_tipo(?)', [tipo], (err, resultados) => {
+        if (err) {
+            results.status(500).json({ message: err.message });
+            return;
+        }
+        if (resultados.length > 0 && resultados[0].length > 0) {
+            results.json(resultados[0]);
+        } else {
+            results.status(404).json({ message: 'Usuario no encontrado' });
+            return; 
+        }
+    });
+});
+
+
+
 api.get('/',(request, results)=>{
     results.send("<h1>Api con express</h1>")
 })
@@ -95,6 +116,46 @@ api.post('/register', (request, results) => {
 });
 
 
+// http://localhost:3000/agregar_platos
+api.post('/agregar_platos', (request, results) => {
+    const {tipo,name,descripciones,imagenes} = request.body;
+  
+    db.query('CALL set_agregar_platos(?,?,?,?)', [tipo,name,descripciones,imagenes], (err, resultados) => {
+        if (err) {
+            results.status(500).json({ message: err.message });
+            return;
+        }
+        results.status(201).json({id:results.insertId , name});
+    });
+});
+p_ 
+
+// http://localhost:3000/get_carrito
+api.post('/get_carrito', (request, results) => {
+    const {token_user} = request.body;
+  
+    db.query('CALL get_carrito(?)', [token_user], (err, resultados) => {
+        if (err) {
+            results.status(500).json({ message: err.message });
+            return;
+        }
+        results.status(201).json({id:results.insertId , name});
+    });
+});
+
+
+// http://localhost:3000/carrito
+api.post('/carrito', (request, results) => {
+    const {menu,user} = request.body;
+  
+    db.query('CALL set_carrito(?,?)', [menu,user], (err, resultados) => {
+        if (err) {
+            results.status(500).json({ message: err.message });
+            return;
+        }
+        results.status(201).json({id:results.insertId , name});
+    });
+});
 
 api.post('/menu', (request,results)=>{
     const {name,descripciones,imagenes} = request.body; 
